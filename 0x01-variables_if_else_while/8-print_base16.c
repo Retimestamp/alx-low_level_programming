c839a12f82b3dba3e9aac0c55396f171867320c6
Retimestamp <retimestamp@gmail.com>
2022-10-14 07:51:33 -0700

    Add print base16 code

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - main function
+* Return: 0 on success
+*/
+
+int main(void)
+{
+	char i = '0';
+
+	for (; i <= '9'; i++)
+		putchar(i);
+	i = 'a';
+	for (; i <= 'f'; i++)
+		putchar(i);
+	putchar(10);
+}
