2f214da098654213495206071d5a9e05426c4cb0
Retimestamp <retimestamp@gmail.com>
2022-10-14 08:01:43 -0700

    Add print comb code

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - main function
+* Return: 0 on success
+*/
+
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(' ');
+		}
+		i++;
+}
+	putchar(10);
+}
