d038703521b96cf77bea6da7589e6ebc045e9120
Retimestamp <retimestamp@gmail.com>
2022-10-14 07:24:25 -0700

    Add print alphabt code

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - main function
+* Return: 0 on success
+*/
+
+int main(void)
+{
+	char i = 'a';
+
+	for (; i <= 'z'; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar(10);
+}
