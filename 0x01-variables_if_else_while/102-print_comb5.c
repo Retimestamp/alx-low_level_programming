4a22342ec495ec9c54bf1e67d64541de4e47cac7
Retimestamp <retimestamp@gmail.com>
2022-10-14 09:47:35 -0700

    Add print comb5 code

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+* main - main function
+* Return: 0 on success
+*/
+
+int main(void)
+{
+	int l = 48;
+	int k = 48;
+	int j = 48;
+	int i = 48;
+
+	for (; l < 58; l++)
+	{
+		for (; k < 58; k++)
+		{
+			for (; j < 58; j++)
+			{
+				for (; i < 58; i++)
+				{
+					if (l != j || i != k)
+					{
+						putchar(l);
+						putchar(k);
+						putchar(' ');
+						putchar(j);
+						putchar(i);
+					}
+					if (l != '9' || k != '8' || j != '9' || i != '9')
+					{
+						putchar(44);
+						putchar(' ');
+					}
+				}
+				i = 48;
+			}
+			i = k + 1;
+			j = l;
+		}
+		k = 48;
+	}
+	putchar(10);
+}
