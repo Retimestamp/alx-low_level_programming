4a39648eacb682eaddfe7abc4ca908b8f1c5009e
Retimestamp <retimestamp@gmail.com>
2022-10-14 08:54:16 -0700

    Add print comb4 code

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+* main - main function
+* Return: 0 on success
+*/
+
+int main(void)
+{
+	int i = 50;
+	int j = 49;
+	int k = 48;
+
+	for (; k < 56; k++)
+	{
+		for (; j < 57; j++)
+		{
+			for (; i < 58; i++)
+			{
+				if (i != j && j != k)
+				{
+					putchar(k);
+					putchar(j);
+					putchar(i);
+					putchar(44);
+					putchar(' ');
+				}
+			}
+			i = j + 1;
+		}
+		j = k + 1;
+		i = j + 1;
+	}
+	putchar(10);
+}
