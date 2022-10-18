2836448462c7c9cca18dfc03416a0ca0f557c353
Retimestamp <retimestamp@gmail.com>
2022-10-18 01:42:00 -0700

    Add main.h and putchar code to functions and nested loops project

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c);
+/**
+* main - main function
+* Return: 0 on success
+*/
+
+int main(void)
+{
+	_putchar(95);
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar(10);
+	return (0);
+}
