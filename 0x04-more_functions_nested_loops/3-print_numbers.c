d087c2dc1654678b665e8a2c4147277b5a546d58
Retimestamp <retimestamp@gmail.com>
2022-10-21 00:06:24 -0700

    Add print numbers code to more functions and nested loops project

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_numbers - prints the numbers 0 to 9
+* Return: void
+*/
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	for (; i < 58; i++)
+		_putchar(i);
+	_putchar(10);
+}
