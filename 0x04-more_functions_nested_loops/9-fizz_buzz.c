cfded6c9709320526976b66138101751e72c55ce
Retimestamp <retimestamp@gmail.com>
2022-10-21 01:59:20 -0700

    Add fizz buzz code to more functions and nested loops project

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - main function
+* Return: 0 on success
+*/
+
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (!(i % 3) && !(i % 5))
+			printf("FizzBuzz");
+		else if (!(i % 3))
+			printf("Fizz");
+		else if (!(i % 5))
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+		i++;
+	}
+	printf("\n");
+}
