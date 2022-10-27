d6d27fbeec89270cd2a06698b961fead04cc3b7a
Retimestamp <retimestamp@gmail.com>
2022-10-27 01:16:33 -0700

    Add string toupper code to more pointers, arrays and strings project

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* string_toupper - changes all lowercase letters of a string to uppercase
+* @s: string input
+* Return: character pointer
+*/
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; *(s + i); i++)
+		if (*(s + i) > 96 && *(s + i) < 124)
+			*(s + i) -= 32;
+	return (s);
+}
